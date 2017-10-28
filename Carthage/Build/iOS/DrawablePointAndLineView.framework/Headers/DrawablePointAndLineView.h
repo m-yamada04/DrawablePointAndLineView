ccc4374dbb76a909282d04bb527fe51fa218552a
Maika Yamada <maika@MaikanoMacBook-Pro.local>
2017-10-28 15:05:59 +0900

    Carthage対応

---
+++ Carthage/Build/iOS/DrawablePointAndLineView.framework/Headers/DrawablePointAndLineView.h
@@ -0,0 +1,19 @@
+//
+//  DrawablePointAndLineView.h
+//  DrawablePointAndLineView
+//
+//  Created by Maika Yamada on 2017/06/11.
+//  Copyright © 2017年 Maika Yamada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawablePointAndLineView.
+FOUNDATION_EXPORT double DrawablePointAndLineViewVersionNumber;
+
+//! Project version string for DrawablePointAndLineView.
+FOUNDATION_EXPORT const unsigned char DrawablePointAndLineViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawablePointAndLineView/PublicHeader.h>
+
+
